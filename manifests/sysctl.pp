c6d4971263c0a98973e9184a4385f98dec5cab07
Andrey Galkin <andrey@futoin.org>
2016-02-24 00:42:42 +0200

    Updated to new sysctl calls

--- manifests/sysctl.pp
+++ manifests/sysctl.pp
@@ -53,14 +53,14 @@ class cfnetwork::sysctl (
     # Turn on Source Address Verification in all interfaces to
     # prevent some spoofing attacks    
     sysctl{ 'net.ipv4.conf.default.rp_filter':
-            value => $rp_filter, permanent=>true }
+            value => $rp_filter}
     sysctl{ 'net.ipv4.conf.all.rp_filter':
-            value => $rp_filter, permanent=>true }
+            value => $rp_filter}
     
     # This disables TCP Window Scaling (http://lkml.org/lkml/2008/2/5/167),
     # and is not recommended.
     sysctl{ 'net.ipv4.tcp_syncookies':
-            value => $tcp_syncookies, permanent=>true }
+            value => $tcp_syncookies}
 
     # Use bridge, if you really need this
     sysctl{ 'net.ipv4.conf.default.proxy_arp': value => 0 }
@@ -69,120 +69,118 @@ class cfnetwork::sysctl (
     
     $enable_routing = $cfnetwork::is_router ? { true => 1, default => 0 };
     sysctl{ 'net.ipv4.conf.default.forwarding':
-            value => $enable_routing, permanent=>true }
+            value => $enable_routing}
     sysctl{ 'net.ipv4.conf.all.forwarding':
-            value => $enable_routing, permanent=>true }
+            value => $enable_routing}
     sysctl{ 'net.ipv6.conf.default.forwarding':
-            value => $enable_routing, permanent=>true }
+            value => $enable_routing}
     sysctl{ 'net.ipv6.conf.all.forwarding':
-            value => $enable_routing, permanent=>true }
+            value => $enable_routing}
     
     
     # ICMP security
     #---
     # Ignore ICMP broadcast
     sysctl{ 'net.ipv4.icmp_echo_ignore_broadcasts':
-            value => 1, permanent=>true }
+            value => 1}
     # Ignore bogus ICMP errors
     sysctl{ 'net.ipv4.icmp_ignore_bogus_error_responses':
-            value => 1, permanent=>true }
+            value => 1}
     
     # Do not accept ICMP redirects (prevent MITM attacks)
     sysctl{ 'net.ipv4.conf.default.accept_redirects':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv4.conf.all.accept_redirects':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv6.conf.default.accept_redirects':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv6.conf.all.accept_redirects':
-            value => 0, permanent=>true }
+            value => 0}
 
     # Do not send ICMP redirects (we are not a router)
     sysctl{ 'net.ipv4.conf.default.send_redirects':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv4.conf.all.send_redirects':
-            value => 0, permanent=>true }
+            value => 0}
 
     # Do not accept IP source route packets (we are not a router)
     sysctl{ 'net.ipv4.conf.default.accept_source_route':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv4.conf.all.accept_source_route':
-            value => 0, permanent=>true }
+            value => 0}
     
     # IPv6 specific
     sysctl{ 'net.ipv6.conf.default.router_solicitations':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv6.conf.default.accept_ra_rtr_pref':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv6.conf.default.accept_ra_pinfo':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv6.conf.default.accept_ra_defrtr':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv6.conf.default.autoconf':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv6.conf.default.dad_transmits':
-            value => 0, permanent=>true }
+            value => 0}
     sysctl{ 'net.ipv6.conf.default.max_addresses':
-            value => 1, permanent=>true }
+            value => 1}
 
     # Optimize network
     #---
     
     # yep, socket related
     sysctl{ 'fs.file-max':
-            value => $file_max, permanent=>true }
+            value => $file_max}
     sysctl{ 'net.core.netdev_max_backlog':
-            value => $netdev_max_backlog, permanent=>true }
+            value => $netdev_max_backlog}
     sysctl{ 'net.core.somaxconn':
-            value => $somaxconn, permanent=>true }
+            value => $somaxconn}
     sysctl{ 'net.core.rmem_max':
-            value => $rmem_max, permanent=>true }
+            value => $rmem_max}
     sysctl{ 'net.core.wmem_max':
-            value => $wmem_max, permanent=>true }
+            value => $wmem_max}
     sysctl{ 'net.core.rmem_default':
-            value => $rmem_default, permanent=>true }
+            value => $rmem_default}
     sysctl{ 'net.core.wmem_default':
-            value => $wmem_default, permanent=>true }
+            value => $wmem_default}
     
     # do not choke on not enough ports
     sysctl{ 'net.ipv4.ip_local_port_range':
-            value => $ip_local_port_range, permanent=>true }
+            value => $ip_local_port_range}
     
     # Basic TCP tuning
     sysctl{ 'net.ipv4.tcp_tw_recycle':
-            value => $tcp_tw_recycle, permanent=>true }
+            value => $tcp_tw_recycle}
     sysctl{ 'net.ipv4.tcp_tw_reuse':
-            value => $tcp_tw_reuse, permanent=>true }
+            value => $tcp_tw_reuse}
     sysctl{ 'net.ipv4.tcp_fin_timeout':
-            value => $tcp_fin_timeout, permanent=>true }
+            value => $tcp_fin_timeout}
     sysctl{ 'net.ipv4.tcp_slow_start_after_idle':
-            value => $tcp_slow_start_after_idle, permanent=>true }
+            value => $tcp_slow_start_after_idle}
     sysctl{ 'net.ipv4.tcp_window_scaling':
-            value => $tcp_window_scaling, permanent=>true }
+            value => $tcp_window_scaling}
     sysctl{ 'net.ipv4.tcp_timestamps':
-            value => $tcp_timestamps, permanent=>true }
+            value => $tcp_timestamps}
     sysctl{ 'net.ipv4.tcp_sack':
-            value => $tcp_sack, permanent=>true }
+            value => $tcp_sack}
     sysctl{ 'net.ipv4.tcp_no_metrics_save':
-            value => $tcp_no_metrics_save, permanent=>true }
+            value => $tcp_no_metrics_save}
     sysctl{ 'net.ipv4.tcp_synack_retries':
-            value => $tcp_synack_retries, permanent=>true }
+            value => $tcp_synack_retries}
     sysctl{ 'net.ipv4.tcp_rfc1337':
-            value => $tcp_rfc1337, permanent=>true }
+            value => $tcp_rfc1337}
     sysctl{ 'net.ipv4.tcp_keepalive_time':
-            value => $tcp_keepalive_time, permanent=>true }
+            value => $tcp_keepalive_time}
     sysctl{ 'net.ipv4.tcp_keepalive_probes':
-            value => $tcp_keepalive_probes, permanent=>true }
+            value => $tcp_keepalive_probes}
     sysctl{ 'net.ipv4.tcp_keepalive_intvl':
-            value => $tcp_keepalive_intvl, permanent=>true }
+            value => $tcp_keepalive_intvl}
     sysctl{ 'net.ipv4.tcp_max_syn_backlog':
-            value => $tcp_max_syn_backlog, permanent=>true }
+            value => $tcp_max_syn_backlog}
     sysctl{ 'net.ipv4.tcp_rmem':
-            value     => "4096 ${tcp_rmem_default} ${tcp_rmem_max}",
-            permanent =>true }
+            value     => "4096 ${tcp_rmem_default} ${tcp_rmem_max}" }
     sysctl{ 'net.ipv4.tcp_wmem':
-            value     => "4096 ${tcp_wmem_default} ${tcp_wmem_max}",
-            permanent =>true }
+            value     => "4096 ${tcp_wmem_default} ${tcp_wmem_max}" }
 
     
     # Avoid firewall on bridge traffic - make a router instead,
@@ -197,17 +195,14 @@ class cfnetwork::sysctl (
         }
         sysctl{ 'net.bridge.bridge-nf-call-ip6tables':
                 value     => 0,
-                permanent =>true,
                 require   => Exec['load_bridge_module'],
         }
         sysctl{ 'net.bridge.bridge-nf-call-iptables':
                 value     => 0,
-                permanent =>true,
                 require   => Exec['load_bridge_module']
         }
         sysctl{ 'net.bridge.bridge-nf-call-arptables':
                 value     => 0,
-                permanent =>true,
                 require   => Exec['load_bridge_module'] }
     }
     
@@ -221,81 +216,66 @@ class cfnetwork::sysctl (
     if $nf_conntrack_max {
         sysctl{ 'net.netfilter.nf_conntrack_max':
             value     => $nf_conntrack_max,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
         }
     }
     if $nf_conntrack_expect_max {
         sysctl{ 'net.netfilter.nf_conntrack_expect_max':
             value     => $nf_conntrack_max,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
         }
     }
     
     sysctl{ 'net.netfilter.nf_conntrack_generic_timeout':
             value     => $nf_conntrack_generic_timeout,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_timeout_syn_sent':
             value     => $nf_conntrack_tcp_timeout_syn_sent,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_timeout_syn_recv':
             value     => $nf_conntrack_tcp_timeout_syn_recv,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_timeout_established':
             value     => $nf_conntrack_tcp_timeout_established,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_timeout_fin_wait':
             value     => $tcp_fin_timeout,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_timeout_last_ack':
             value     => $nf_conntrack_tcp_timeout_last_ack,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_timeout_time_wait':
             value     => $nf_conntrack_tcp_timeout_time_wait,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_loose':
             value     => $nf_conntrack_tcp_loose,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_be_liberal':
             value     => $nf_conntrack_tcp_be_liberal,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_tcp_max_retrans':
             value     => $nf_conntrack_tcp_max_retrans,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_udp_timeout':
             value     => $nf_conntrack_udp_timeout,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_udp_timeout_stream':
             value     => $nf_conntrack_udp_timeout_stream,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
     sysctl{ 'net.netfilter.nf_conntrack_icmp_timeout':
             value     => $nf_conntrack_icmp_timeout,
-            permanent =>true,
             require   => Exec['load_conntrack_module'],
     }
 }
